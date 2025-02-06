e7be49c7d38098286b974f24cb217c33fb8a9a40
Withoutwaxwqy <2137697992@qq.com>
2025-02-06 11:24:18 +0800

    add allan plot

--- IMU/allan/allan.py
+++ IMU/allan/allan.py
@@ -22,6 +22,8 @@ class allan:
             return self.standard_allan()
         elif self.mode == "Overlap":
             return self.overlap_allan()
+        elif self.mode == "half_overlap":
+            return self.half_overlap_allan()
         elif self.mode == "Equal_interval":
             return self.equal_interval_allan()
         
@@ -30,41 +32,101 @@ class allan:
         Standard Allan Variance
         """
         y = self.data
-        if np.ndim(y) == 1:
-            y = np.array(y.tolist())
-            imufreq = self.imufreq
-            tau0 = 1/imufreq
-            N = len(y)
-            NL = N
-            # calculate the standard deviation of the data
-            sigma = []
-            tau = []
-            # calculate the Allan variance
-            for k in range(1, 1000):
-                sigma_k = np.sqrt(1/(2*(NL-1))*np.sum((y[1:NL]-y[0:(NL-1)])**2))
-                sigma.append(sigma_k)
-                tau_k = 2 ** (k-1) * tau0
-                tau.append(tau_k)
+        y = __modify_imu(y)
+        y = np.array(y.tolist())
+        imufreq = self.imufreq
+        tau0 = 1/imufreq
+        N = len(y)
+        NL = N
+        # calculate the standard deviation of the data
+        sigma = []
+        tau = []
+        # calculate the Allan variance
+        for k in range(1, 1000):
+            sigma_k = np.sqrt(1/(2*(NL-1))*np.sum((y[1:NL]-y[0:(NL-1)])**2))
+            sigma.append(sigma_k)
+            tau_k = 2 ** (k-1) * tau0
+            tau.append(tau_k)
 
-                NL = int(bp.floor(NL/2))
-                if NL < 3:
-                    break
-                y = 1/2*(y[0:NL*2:2]+y[1:NL*2:2])
-            self.tau = tau
-            self.sigma = sigma
+            NL = int(bp.floor(NL/2))
+            if NL < 3:
+                break
+            y = 1/2*(y[0:NL*2:2]+y[1:NL*2:2])
+        self.tau = tau
+        self.sigma = sigma
+    
+    def half_overlap_allan(self):
+        """
+        Half Overlap Allan Variance
+        """
+        y = self.data
+        y = __modify_imu(y)
+    
+        imufreq = self.imufreq
+        tau0 = 1/imufreq
+        N = len(y)
+        NL = N
+        # calculate the standard deviation of the data
+        sigma = []
+        tau = []
+        # calculate the Allan variance
+        for k in range(1, 1000):
+            sigma_k = np.sqrt(1/(2*(NL-1))*np.sum((y[1:NL]-y[0:(NL-1)])**2))
+            sigma.append(sigma_k)
+            tau_k = 2 ** (k-1) * tau0
+            tau.append(tau_k)
 
-        if np.ndim(y) == 2:
-            if y.shape[0] > 100 and y.shape[1] == 6:
-                y = y
-            elif y.shape[0] == 6 and y.shape[1] > 100:
-                y = y.T
-            elif y.shape[0] == 7 and y.shape[1] == 100:
-                y = y[1:, :]
-                y=y.T
-            elif y.shape[0] == 100 and y.shape[1] == 7:
-                y = y[:, 1:]
-            else:
-                raise ValueError("IMU data error! Please check the input data.should be [Time* GYRX Y Z ACCX Y Z] (Time optional)")
-            for i in range(6):
-                yy = y[:, i]
-                
+            NL = int(bp.floor(NL/2))
+            if NL < 3:
+                break
+            y = 1/2*(y[0:NL*2:2]+y[1:NL*2:2])
+        self.tau = tau
+        self.sigma = sigma
+
+
+    def overlap_allan(self):
+        """
+        Overlap Allan Variance
+        """
+        y = self.data
+        y = __modify_imu(y)
+        imufreq = self.imufreq
+        tau0 = 1/imufreq
+        N = len(y)
+        NL = N
+        # calculate the standard deviation of the data
+        sigma = []
+        tau = []
+        # calculate the Allan variance
+        for k in range(1, 1000):
+            sigma_k = np.sqrt(1/(2*(NL-1))*np.sum((y[1:NL]-y[0:(NL-1)])**2))
+            sigma.append(sigma_k)
+            tau_k = 2 ** (k-1) * tau0
+            tau.append(tau_k)
+
+            NL = int(bp.floor(NL/2))
+            if NL < 3:
+                break
+            y = 1/2*(y[0:NL*2:2]+y[1:NL*2:2])
+        self.tau = tau
+        self.sigma = sigma
+
+
+
+
+def __modify_imu(data):
+    if np.ndim(data) == 1:
+        data = np.array(data.tolist())
+    if np.ndim(data) == 2:
+        if data.shape[0] > 100 and data.shape[1] == 6:
+            data = data
+        elif data.shape[0] == 6 and data.shape[1] > 100:
+            data = data.T
+        elif data.shape[0] == 7 and data.shape[1] == 100:
+            data = data[1:, :]
+            data = data.T
+        elif data.shape[0] == 100 and data.shape[1] == 7:
+            data = data[:, 1:]
+        else:
+            raise ValueError("IMU data error! Please check the input data.should be [Time* GYRX Y Z ACCX Y Z] (Time optional)")
+    return data
--- README.md
+++ README.md
@@ -14,6 +14,11 @@
 - `GUI/`: 包含图形用户界面相关的模块。
   - `logTimeLine.py`: 时间线日志相关的 GUI 模块。
   - `__init__.py`: 使目录成为一个 Python 包。
-- `test.py`: 测试脚本。
+
+- `plot/` : 绘图脚本
+  - `trajectory_animation.py` 绘制动画
+- `IMU/`: IMU建模 标定 测试 以及性能
+  - `allan/allan.py` :allan 方差相关
 - `README.md`: 项目说明文件。
 - `requirements.txt`: 项目依赖文件。
+- `test.py`: 测试脚本。
